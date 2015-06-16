78d20ac73a9854ea7bdbb6b6c49b586f05837c63
James Montalvo <jamesmontalvo3@gmail.com>
2015-06-16 10:35:47 -0500

    add gitignore; working on setup script

--- client_files/setup.sh
+++ client_files/setup.sh
@@ -1,4 +1,9 @@
-
+#
+# To get this running on CentOS 6.6 minimal install, perform the following:
+#   ifup eth0
+#   yum -y install wget
+#   wget -O - https://raw.githubusercontent.com/enterprisemediawiki/Meza1/scripted/client_files/setup.sh | bash
+#
 
 #
 # Modify network scripts in /etc/sysconfig/network-scripts, 
@@ -7,16 +12,16 @@
 cd /etc/sysconfig/network-scripts
 
 # modify ifcfg-eth0 (NAT)
-sed -r --in-place 's/ONBOOT=no/ONBOOT=yes/g;' ./ifcfg-eth0.txt
-sed -r --in-place 's/NM_CONTROLLED=yes/NM_CONTROLLED=no/g;' ./ifcfg-eth0.txt
+sed -r -i 's/ONBOOT=no/ONBOOT=yes/g;' ./ifcfg-eth0
+sed -r -i 's/NM_CONTROLLED=yes/NM_CONTROLLED=no/g;' ./ifcfg-eth0
 
-# get eth1 HWADDR
+# get eth1 HWADDR from ifconfig
 eth1_hwaddr="$(ifconfig eth1 | grep '[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}' -o -P)"
 
 # modify ifcfg-eth1 (host-only)
 cp ./ifcfg-eth0 ./ifcfg-eth1
-sed -r --in-place "s/HWADDR=.*$/HWADDR=$eth1_hwaddr\nIPADDR=192.168.56.56\nNETMASK=255.255.255.0/g;" ./ifcfg-eth1
-sed -r --in-place 's/BOOTPROTO=dhcp/BOOTPROTO=static/g;' ./test.txt
+sed -r -i "s/HWADDR=.*$/HWADDR=$eth1_hwaddr\nIPADDR=192.168.56.56\nNETMASK=255.255.255.0/g;" ./ifcfg-eth1
+sed -r -i 's/BOOTPROTO=dhcp/BOOTPROTO=static/g;' ./ifcfg-eth1
 
 # restart networking
 service network restart
@@ -27,15 +32,20 @@ service network restart
 yum -y update
 
 #
+# Get development tools
+#
+yum groupinstall -y development
+yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel xz-libs openssh-server openssh-clients
+
+#
 # Setup SSH
 #
-yum -y install openssh-server openssh-clients
 chkconfig sshd on
 service sshd start
 
 
 #
-# Configure IPTABLES
+# Configure IPTABLES to open port 80 (for Apache HTTP)
 # @todo: consider method to define entire iptables config:
 # http://blog.astaz3l.com/2015/03/06/secure-firewall-for-centos/
 #
