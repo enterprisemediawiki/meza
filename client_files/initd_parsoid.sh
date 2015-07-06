a9a9d3930fb99cc6f4f5a2a90f6039bc11c4330b
James Montalvo <edwin.j.montalvo@nasa.gov>
2015-07-06 13:04:51 -0500

    remove lock file, check for process ID

--- client_files/initd_parsoid.sh
+++ client_files/initd_parsoid.sh
@@ -5,6 +5,7 @@
 # description: Node.js /etc/parsoid/api/server.js
 #
 
+# provides echo_success and echo_failure functions (amongst others)
 . /etc/rc.d/init.d/functions
 
 USER="parsoid"
@@ -17,31 +18,39 @@ LOG_FILE="$ROOT_DIR/server.js.log"
 
 LOCK_FILE="/var/lock/subsys/node-server"
 
+get_parsoid_process_id()
+{
+	echo `ps -aefw | grep "$DAEMON $SERVER" | grep -v " grep " | awk '{print $2}'`
+}
+
+
+
+parsoid_start_echo_success()
+{
+	# Wait 10 seconds since paroid takes some time to start and we don't have a
+	# good method to be told when it's up
+	sleep 10
+	echo_success # normal echo_success function
+}
 do_start()
 {
-        if [ ! -f "$LOCK_FILE" ] ; then
-                echo -n $"Starting $SERVER: "
+    pid=$(get_parsoid_process_id)
+    echo "Current parsoid process ID: $pid (end pid)"
 
-                # Use "nohup" to prevent hang-up. Thanks to:
-                # http://stackoverflow.com/questions/5818202/how-to-run-node-js-app-forever-when-console-is-closed
-                runuser -l "$USER" -c "nohup $DAEMON $SERVER >> $LOG_FILE &" && echo_success || echo_failure
+    echo -n $"Starting $SERVER: "
 
-                RETVAL=$?
-                echo
-                [ $RETVAL -eq 0 ] && touch $LOCK_FILE
-        else
-                echo "$SERVER is locked."
-                RETVAL=1
-        fi
+    # Use "nohup" to prevent hang-up. Thanks to:
+    # http://stackoverflow.com/questions/5818202/how-to-run-node-js-app-forever-when-console-is-closed
+    runuser -l "$USER" -c "nohup $DAEMON $SERVER >> $LOG_FILE &" && parsoid_start_echo_success || echo_failure
+    RETVAL=$?
 }
 do_stop()
 {
-        echo -n $"Stopping $SERVER: "
-        pid=`ps -aefw | grep "$DAEMON $SERVER" | grep -v " grep " | awk '{print $2}'`
-        kill -9 $pid > /dev/null 2>&1 && echo_success || echo_failure
-        RETVAL=$?
-        echo
-        [ $RETVAL -eq 0 ] && rm -f $LOCK_FILE
+    echo -n $"Stopping $SERVER: "
+    # pid=`ps -aefw | grep "$DAEMON $SERVER" | grep -v " grep " | awk '{print $2}'`
+    pid=$(get_parsoid_process_id)
+    kill -9 $pid > /dev/null 2>&1 && echo_success || echo_failure
+    RETVAL=$?
 }
 
 case "$1" in
