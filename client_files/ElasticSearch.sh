cd3600f18553fc1c9e2e5626b18223dd6d0b2f13
Daren Welsh <darenwelsh@gmail.com>
2015-07-06 18:06:02 -0500

    got it to work, but script needs more work

--- client_files/ElasticSearch.sh
+++ client_files/ElasticSearch.sh
@@ -31,12 +31,6 @@ echo "JAVA_HOME=\"/usr/bin\"" >> /etc/environment
 source /etc/environment
 echo $JAVA_HOME 
 
-#
-# Install Elasticsearch
-#
-# https://www.elastic.co/guide/en/elasticsearch/reference/current/_installation.html
-#
-
 # Install Elasticsearch via yum repository
 #
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-repositories.html
@@ -50,7 +44,7 @@ rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 # Add yum repo file
 echo "******* Downloading yum repo file for Elasticsearch *******"
 cd ~/sources/meza1/client_files
-wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/elasticsearch.repo
+#wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/elasticsearch.repo
 cp ./elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
 
 # Install repo
@@ -77,17 +71,27 @@ chkconfig --add elasticsearch
 # Add host name per https://github.com/elastic/elasticsearch/issues/6611
 echo "127.0.0.1 Meza1" >> /etc/hosts
 
+# Rename the standard config file and copy over our custom config file
+cd /etc/elasticsearch
+mv ./elasticsearch.yml ./elasticsearch-old.yml
 cd ~/sources/meza1/client_files
-wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/elasticsearch.yml
-cp ./elasticsearch.yml /etc/elasticsearch-1.6.0/config
-# create a config file and copy it in elasticsearch.yml
+# wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/elasticsearch.yml
+cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
-
-# Manually start Elasticsearch
-# /usr/share/elasticsearch/bin/elasticsearch #add -d to run in background
-
-# TODO: Start elasticsearch at this point or is there configuration to add?
-# service elasticsearch start #options:  --cluster.name my_cluster_name --node.name my_node_name
+# Make directories called out in elasticsearch.yml
+# ref: http://elasticsearch-users.115913.n3.nabble.com/Elasticsearch-Not-Working-td4059398.html
+cd /var
+mkdir data
+cd data
+mkdir elasticsearch
+cd /var
+mkdir work
+cd work
+mkdir elasticsearch
+cd /var
+# Grant elasticsearch user ownership of these new directories
+chown -R elasticsearch /var/data/elasticsearch
+chown -R elasticsearch /var/work/elasticsearch
 
 #
 # Install Extension:Elastica and Extension:CirrusSearch
@@ -96,7 +100,7 @@ echo "******* Downloading extensions *******"
 cd ~/sources/meza1/client_files
 # Add Elastica and CirrusSearch to ExtensionSettings
 # TODO: This part can be modified if the user gets this file in the initial VM setup script
-wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/ExtensionSettingsElasticSearch.php
+#wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/ExtensionSettingsElasticSearch.php
 cp ~/sources/meza1/client_files/ExtensionSettingsElasticSearch.php /var/www/meza1/htdocs/wiki/ExtensionSettingsElasticSearch.php
 cat /var/www/meza1/htdocs/wiki/ExtensionSettingsElasticSearch.php >> /var/www/meza1/htdocs/wiki/ExtensionSettings.php
 
@@ -107,22 +111,46 @@ cat /var/www/meza1/htdocs/wiki/ExtensionSettingsElasticSearch.php >> /var/www/me
 # Add CirrusSearch settings to LocalSettings.php
 echo "******* Downloading configuration files *******"
 cd ~/sources/meza1/client_files
-wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/LocalSettingsElasticSearch.php
+#wget https://raw.githubusercontent.com/enterprisemediawiki/Meza1/installElasticSearch/client_files/LocalSettingsElasticSearch.php
 cp ~/sources/meza1/client_files/LocalSettingsElasticSearch.php /var/www/meza1/htdocs/wiki/LocalSettingsElasticSearch.php
 cat /var/www/meza1/htdocs/wiki/LocalSettingsElasticSearch.php >> /var/www/meza1/htdocs/wiki/LocalSettings.php
 
+# Run updateExtensions to install UniversalLanguageSelector and VisualEditor
+echo "******* Installing extensions *******"
+php /var/www/meza1/htdocs/wiki/extensions/ExtensionLoader/updateExtensions.php
+# Install Elastica library via composer
+cd /var/www/meza1/htdocs/wiki/extensions/Elastica
+composer install
+
+# Any time you run updateExtensions.php it may be required to run
+# `php maintenance/update.php` since new extension versions may be installed
+echo "******* Running update.php to update database as required *******"
+cd /var/www/meza1/htdocs/wiki/maintenance
+php update.php --quick
+
+# Start Elasticsearch
+service elasticsearch start
+
+# Incorporate steps from https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FCirrusSearch.git/REL1_25/README
+#Add this to LocalSettings.php:
+# $wgDisableSearchUpdate = true;
+ 
+# Now run this script to generate your elasticsearch index:
+php /var/www/meza1/htdocs/wiki/extensions/CirrusSearch/maintenance/updateSearchIndexConfig.php
+ 
+# Now remove $wgDisableSearchUpdate = true from LocalSettings.php.  Updates should start heading to Elasticsearch.
+ 
+#Next bootstrap the search index by running:
+# php $MW_INSTALL_PATH/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipLinks --indexOnSkip
+# php $MW_INSTALL_PATH/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipParse
+#Note that this can take some time.  For large wikis read "Bootstrapping large wikis" below.
+ 
+#Once that is complete add this to LocalSettings.php to funnel queries to ElasticSearch:
+# $wgSearchType = 'CirrusSearch';
 
 # Lots of things to add from
 # https://www.mediawiki.org/wiki/Extension:CirrusSearch
 # https://en.wikipedia.org/w/api.php?action=cirrus-config-dump&srbackend=CirrusSearch&format=json
 # https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FCirrusSearch.git/REL1_25/README
 #
-
-# Run updateExtensions to install UniversalLanguageSelector and VisualEditor
-echo "******* Installing extensions *******"
-php /var/www/meza1/htdocs/wiki/extensions/ExtensionLoader/updateExtensions.php
-# Not sure if composer install is required - see: https://www.mediawiki.org/wiki/Extension:Elastica
-# composer install
-
-
 echo "******* Complete! *******"
