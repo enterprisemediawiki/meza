ec28e74bcdbd6f7a4686fb6ceae048af4d8adbb0
James Montalvo <jamesmontalvo3@gmail.com>
2015-08-10 14:31:19 -0500

    Change Elastic Search extensions repo to gerrit

--- client_files/ExtensionSettingsElasticSearch.php
+++ client_files/ExtensionSettingsElasticSearch.php
@@ -2,11 +2,11 @@
 # Add 2 extensions required for Elastic Search
 $egExtensionLoaderConfig += array(
 	'Elastica' => array(
-		'git' => 'https://git.wikimedia.org/git/mediawiki/extensions/Elastica.git',
+		'git' => 'https://gerrit.wikimedia.org/r/mediawiki/extensions/Elastica.git',
 		'branch' => 'REL1_25',
 	),
 	'CirrusSearch' => array(
-		'git' => 'https://git.wikimedia.org/git/mediawiki/extensions/CirrusSearch.git',
+		'git' => 'https://gerrit.wikimedia.org/r/mediawiki/extensions/CirrusSearch.git',
 		'branch' => 'REL1_25',
 	),
 );
