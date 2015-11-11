3d4ce4805eb6191b62c4501e5d1d62c439a81c23
James Montalvo <edwin.j.montalvo@nasa.gov>
2015-11-11 08:53:18 -0600

    use version of ExtensionLoader supporting tags; SummaryTimeline checkout tag

--- scripts/extensions.sh
+++ scripts/extensions.sh
@@ -28,7 +28,7 @@ echo -e "\n\n## meza: Install ExtensionLoader and apply changes to MW settings"
 cd "$m_mediawiki/extensions"
 git clone https://github.com/jamesmontalvo3/ExtensionLoader.git
 cd ./ExtensionLoader
-git checkout tags/v0.2.1
+git checkout tags/v0.2.2
 cd "$m_mediawiki"
 
 
