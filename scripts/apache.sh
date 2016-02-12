f524f5f72720f4f68509085efa7d659cf59c0dcf
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-02-12 17:16:16 -0600

    Fix logrotate issue (hopefully) and remove to-do's from apache.sh
    Addresses issues #138 and #175

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -10,24 +10,7 @@ print_title "Starting script apache.sh"
 chown -R apache:apache "$m_htdocs"
 chmod -R 775 "$m_htdocs"
 
-
-#
-# Skip section (not titled) on httpd.conf "Supplemental configuration"
-# Skip section titled "httpd-mpm.conf"
-# Skip section titled "Vhosts for apache 2.4.12"
-#
-# @todo: figure out if this section is necessary For now skip section titled "httpd-security.conf"
-#
-
-# @todo: pick up from section "Modify config file"
-
-#### NOT YET COMPLETE ####
-
-
-
-
 cd /etc/httpd/conf
-
 
 # rename default configuration file, get meza config file
 mv httpd.conf httpd.default.conf
@@ -36,22 +19,9 @@ cp "$m_meza/scripts/config/httpd.conf" ./httpd.conf
 # replace INSERT-DOMAIN-OR-IP with domain...or IP address
 sed -r -i "s/INSERT-DOMAIN-OR-IP/$mw_api_domain/g;" ./httpd.conf
 
-
-
-# create service script - THIS SHOULD BE DONE BY YUM NOW
-# cd /etc/init.d
-# cp "$m_meza/scripts/initd_httpd.sh" ./httpd
-# chmod +x /etc/init.d/httpd
-
-
 # create logrotate file
-# THIS MAY BE DONE BY YUM, BUT SINCE THESE FILES ARE REFERENCED IN OUR
-# CUSTOM httpd.conf WE SHOULD STICK WITH THIS FOR NOW. OUR CUSTOM httpd.conf
-# MAY REQUIRE REVISION.
 cd /etc/logrotate.d
 cp "$m_meza/scripts/logrotated_httpd" ./httpd
-
-cd "$m_htdocs"
 
 
 # modify firewall rules
