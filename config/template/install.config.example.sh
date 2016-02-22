352144765c399c5b8ab86d8270701e1a90d4d12b
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-02-22 07:07:10 -0600

    Move prompts back into install.sh because...
    Turns out that install_prompts.sh is not available if you
    haven't pulled the whole meza repo yet. Oops.

--- config/template/install.config.example.sh
+++ config/template/install.config.example.sh
@@ -2,6 +2,7 @@
 #
 # File to setup install.sh without using prompts
 
+
 git_branch="master"
 
 usergithubtoken="e9191bc6d394d64011273d19f4c6be47eb10e25b"
@@ -12,8 +13,6 @@ mw_api_domain="192.168.56.56"
 
 mediawiki_git_install="y"
 
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/C=US/ST=TX/L=Houston/O=EnterpriseMediaWiki/CN=enterprisemediawiki.org" \
-    -keyout /etc/pki/tls/private/meza.key -out /etc/pki/tls/certs/meza.crt
+openssl_self_sign_subject="/C=US/ST=TX/L=Houston/O=EnterpriseMediaWiki/CN=enterprisemediawiki.org"
 
 slackwebhook="n"
--- scripts/install.sh
+++ scripts/install.sh
@@ -45,8 +45,142 @@ else
     enterprise_linux_version=6
 fi
 
-# Perform prompts
-source "$m_meza/scripts/install_prompts.sh"
+
+# Check for install config file before prompts
+if [[ ! -z "$1" ]]; then
+	if [[ ! -f "$1" ]]; then
+		echo
+		echo "$1 is not a file. Exiting."
+		exit 1
+	else
+		source "$1"
+	fi
+fi
+
+
+# # # # # # # # # #
+#  BEGIN PROMPTS  #
+# # # # # # # # # #
+
+# Prompt user for git branch
+default_git_branch="master"
+
+while [ -z "$git_branch" ]; do
+
+	echo -e "\n\nType the git branch of meza you want to use and press [ENTER]:"
+	read -e -i $default_git_branch git_branch
+	git_branch=${git_branch:-$default_git_branch}
+
+done
+
+
+# Prompt user for GitHub API personal access token
+default_usergithubtoken="e9191bc6d394d64011273d19f4c6be47eb10e25b" # From Oscar Rogers
+
+while [ -z "$usergithubtoken" ]; do
+
+	echo -e "\nIf you run this script multiple times from one IP address,"
+	echo -e "you might exceed GitHub's API rate limit."
+	echo -e "\nYou may just press [ENTER] to use our generic token (which may exceed limits if used too much) or"
+	echo -e "Visit https://github.com/settings/tokens to generate a new token (with no scopes)."
+	echo -e "and copy/paste your 40-character token and press [ENTER]: "
+	read usergithubtoken
+	usergithubtoken=${usergithubtoken:-$default_usergithubtoken}
+
+done
+
+
+# Prompt user for MySQL password
+default_mysql_root_pass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+
+while [ -z "$mysql_root_pass" ]; do
+
+	echo -e "\nType your desired MySQL root password"
+	echo -e "or leave blank for a randomly generated password and press [ENTER]:"
+	read -s mysql_root_pass
+	mysql_root_pass=${mysql_root_pass:-$default_mysql_root_pass}
+
+done
+
+
+# Prompt user for MW API Domain or IP address
+while [ -z "$mw_api_domain" ]; do
+
+	# This for loop attempts to find the correct network adapter from which to pull the domain or IP address
+	# If multiple adapters are configured (as in our VirtualBox configs), put the most-likely correct one last
+	for networkadapter in eth0 eth1 enp0s3 enp0s8
+	do
+		if [ -n "ip addr | grep $networkadapter | awk 'NR==2 { print $2 }' | awk '-F[/]' '{ print $1 }'" ]; then
+			default_mw_api_domain="`ip addr | grep $networkadapter | awk 'NR==2 { print $2 }' | awk '-F[/]' '{ print $1 }'`"
+		fi
+	done
+
+	echo -e "\nType domain or IP address of your wiki and press [ENTER]:"
+	# If the above logic found a value to use as a default suggestion, display it and still prompt user for value
+	if [ -n "$default_mw_api_domain" ]; then
+	        read -e -i $default_mw_api_domain mw_api_domain
+	# If the above logic did not find a value to suggest, only read the value in (this fixes #238)
+	else
+	        read -e mw_api_domain
+	fi
+	mw_api_domain=${mw_api_domain:-$default_mw_api_domain}
+
+done
+
+
+# Prompt user for MW install method
+default_mediawiki_git_install="y"
+
+while [ -z "$mediawiki_git_install" ]; do
+
+	echo -e "\nInstall MediaWiki with git? (y/n) [ENTER]:"
+	read -e -i $default_mediawiki_git_install mediawiki_git_install
+	mediawiki_git_install=${mediawiki_git_install:-$default_mediawiki_git_install}
+
+done
+
+
+# Prompt for SSL self-signed certificate info
+if [ -z "$openssl_self_sign_subject" ]; then
+
+	echo
+	echo "Next you're going to setup your self-signed certificate for https."
+	echo "Enter values for each of the following fields. Hit any key to continue."
+	read -s dummy # is there another way to do this?
+
+	# generate a self-signed SSL signature (for swap-out of a good signature later, of course!)
+	openssl req -newkey rsa:4096 -nodes -keyout /etc/pki/tls/private/meza.key -x509 -days 365 -out /etc/pki/tls/certs/meza.crt
+
+else
+
+	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+	    -subj "$openssl_self_sign_subject" \
+	    -keyout /etc/pki/tls/private/meza.key -out /etc/pki/tls/certs/meza.crt
+
+fi
+
+
+# Prompt for Slack webhook
+if [[ "$slackwebhook" != "n" ]]; then
+
+	echo
+	echo
+	echo "Announce completion on Slack?"
+	echo "Enter webhook URI or leave blank to opt out:"
+	read slackwebhook
+
+fi
+
+if [[ -z "$slackwebhook" ]]; then
+	slackwebhook="n"
+fi
+
+# # # # # # # #
+# END PROMPTS #
+# # # # # # # #
+
+
+
 
 # Prompt user for MW API protocol -- ASSUME HTTPS. Perhaps we'll remove this assumption later
 # default_mw_api_protocol="http"
