0bcf6e3824833dea9a0c2ada19651cb517575c1b
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-02-22 18:09:34 -0600

    Remove NASA-specific extensions; provide way to customize extensions
    * Removed IMSQuery and SummaryTimeline
    * Added template config file for loading additional non-meza extensions

---
+++ config/template/more-extensions.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *  Additional extensions for meza
+ *
+ *  While you can choose to load additional extensions into meza however you'd,
+ *  this is one method. Move this file into `/opt/meza/config/local` and rename
+ *  it to `overrides.php`. If you already have `overrides.php` and would prefer
+ *  not to add all this content to it, pick another name and `require_once` the
+ *  file.
+ *
+ *  In the file below five other extensions are loaded, one by default and four
+ *  only if certain variables are set to `true`. Enabling these four extensions
+ *  can be done in each wiki's `setup.php` file (not the all-wiki `setup.php`)
+ *
+ **/
+
+
+#
+# Extension:IMSQuery
+#
+require_once $egExtensionLoader->registerLegacyExtension(
+	"IMSQuery",
+	"https://github.com/jamesmontalvo3/IMSQuery.git",
+	"master"
+);
+
+
+if ( isset( $mezaLoadSummaryTimeline ) && $mezaLoadSummaryTimeline ) {
+
+	#
+	# Extension:SummaryTimeline
+	#
+	require_once $egExtensionLoader->registerLegacyExtension(
+		"SummaryTimeline",
+		"https://github.com/darenwelsh/SummaryTimeline.git",
+		"tags/0.2.0"
+	);
+
+}
+
+
+if ( isset( $mezaLoadTOPO ) && $mezaLoadTOPO ) {
+
+	#
+	# Extension:HideSubPage
+	#
+	require_once $egExtensionLoader->registerLegacyExtension(
+		"HideSubPage",
+		"https://github.com/emanspeaks/HideSubPage.git",
+		"master"
+	);
+
+	#
+	# Extension:CrossReference
+	#
+	require_once $egExtensionLoader->registerLegacyExtension(
+		"CrossReference",
+		"https://github.com/jamesmontalvo3/CrossReference.git",
+		"master"
+	);
+
+	#
+	# Extension:TreeAndMenu
+	#
+	require_once $egExtensionLoader->registerLegacyExtension(
+		"TreeAndMenu",
+		"https://github.com/jamesmontalvo3/TreeAndMenu.git",
+		"master"
+	);
+
+
+	$wgHooks['BeforePageDisplay'][] = 'wfAddSidebarTree';
+	function wfAddSidebarTree( $out, $skin ) {
+		$title = Title::newFromText( 'SidebarTree', NS_MEDIAWIKI );
+		$article = new Article( $title );
+		$html = $out->parse( $article->getContent() );
+		$out->addHTML( "<div id=\"wikitext-sidebar\">$html</div>" );
+		return true;
+	}
+
+}
