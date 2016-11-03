a0d5b5515017eb81537cc85174e0b946c4995925
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-11-03 11:43:05 -0500

    don't call out opcache in php.ini since yum seems to do it somehow, don't install pear for now

--- scripts/php.sh
+++ scripts/php.sh
@@ -51,9 +51,9 @@ chkconfig httpd on
 service httpd status
 service httpd restart
 
-# Install PEAR and PEAR Mail
-chmod 744 "$m_meza/scripts/install-pear.sh"
-"$m_meza/scripts/install-pear.sh"
-/usr/local/php/bin/pear install --alldeps Mail
+# # Install PEAR and PEAR Mail
+# chmod 744 "$m_meza/scripts/install-pear.sh"
+# "$m_meza/scripts/install-pear.sh"
+# /usr/local/php/bin/pear install --alldeps Mail
 
 echo -e "\n\nPHP has been setup.\n\nPlease use the web browser on your host computer to navigate to http://192.168.56.56/info.php to verify php is being executed."
