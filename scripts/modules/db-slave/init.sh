51266f85de63eb88c72ba229bbd042a7c9e57c99
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-12-16 09:19:17 -0600

    can't put master-host,etc variable into my.cnf anymore. fixes for slave setup

--- scripts/modules/db-slave/init.sh
+++ scripts/modules/db-slave/init.sh
@@ -2,25 +2,8 @@
 #
 # Setup slave server
 
-master_db_ip=`echo "$db_server_ips" | awk '{print $1;}'`
-
-# in my.cnf
-my_cnf_additions=`cat <<EOF
-
-# Setup for DB_SLAVE
-server-id = $db_slave_id
-master-host = $master_db_ip
-master-user = $m_db_slave_user
-master-password = $db_slave_password
-master-connect-retry = 60
-
-EOF`
-echo "$my_cnf_additions" > /root/tmpSlaveDBfileForReplication
-
-# find [mysqld] in my.cnf, add replication info from above
-sed -i -e '/\[mysqld\]/ r /root/tmpSlaveDBfileForReplication' /etc/my.cnf
-rm /root/tmpSlaveDBfileForReplication # remove temp file
-
+# add server ID for DB_SLAVE
+sed -i  "s/\[mysqld\]/\[mysqld\]\n\n# Setup for DB_SLAVE\nserver-id = $db_slave_id/" /etc/my.cnf
 
 # source replication file
 mysql -u root "--password=$mysql_root_pass" < "$m_db_replication_dump_file"
@@ -29,10 +12,11 @@ mysql -u root "--password=$mysql_root_pass" < "$m_db_replication_dump_file"
 systemctl restart mariadb
 
 # Get replication log info transferred from DB_MASTER
-master_log_file=`cat "m_db_replication_log_file"`
-master_log_pos=`cat "m_db_replication_log_pos"`
+master_log_file=`cat "$m_db_replication_log_file"`
+master_log_pos=`cat "$m_db_replication_log_pos"`
 
 # Run queries to setup replication
+master_db_ip=`echo "$db_server_ips" | awk '{print $1;}'`
 query=`cat <<EOF
 	SLAVE STOP;
 	CHANGE MASTER TO MASTER_HOST='$master_db_ip',
@@ -41,9 +25,12 @@ query=`cat <<EOF
 		MASTER_LOG_FILE='$master_log_file',
 		MASTER_LOG_POS=$master_log_pos;
 	START SLAVE;
-	SHOW SLAVE STATUS
 EOF`
 echo
 echo "Running query for slave DB setup"
 mysql -u root "--password=$mysql_root_pass" -e"$query"
 
+slave_status=`mysql -u root "--password=$mysql_root_pass" -e"SHOW SLAVE STATUS\G" | sed -n -e 's/^.*Slave_IO_State: //p'`
+if [ "$slave_status" = "Waiting for master to send event" ]; then
+	echo "Slave status good: $slave_status";
+fi
