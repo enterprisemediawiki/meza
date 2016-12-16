dfad235bdb94d678e98dd9729048a634d173a33b
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-12-16 10:28:14 -0600

    i18n to correct prompts

--- scripts/modules/db-client/prompts.sh
+++ scripts/modules/db-client/prompts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-meza prompt        db_server_ips   "Type domain or IP address (or 'localhost') of master DB"
-meza prompt_secure db_password     "Type password for master DB user"
+meza prompt        db_server_ips   "$MSG_prompt_db_server_ips"
+meza prompt_secure db_password     "$MSG_prompt_db_password"
