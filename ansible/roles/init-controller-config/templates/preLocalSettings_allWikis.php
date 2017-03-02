4bae106b299998749a1766ff4055bb3e173ffe93
James Montalvo <jamesmontalvo3@gmail.com>
2017-03-01 18:29:23 -0600

    create-wiki playbooks with unified create/import/backup model (#478)
    * create-wiki playbooks with unified create/import/backup model
    
    * Re-add missing items in create-wiki process
    
    * Remove update database from create-wiki-wrapper
      * Move to verify-wiki/tasks/init-wiki.yml along with
        several missing items:
        * import meeting minutes pages
        * rebuild recent changes
        * create admin user on demo wiki
        * SMW rebuild data
        * runJobs.php
        * Build elasticsearch index
      * run init-wiki only when a new wiki created.
    * Note: update.php will run twice for Demo Wiki and anytime
      a wiki is added to the config and then deploy is run.
    
    * got a 129 exit code from 'rm -rf ...' command. possible solution
    
    * Need to notify parsoid to restart after new wiki

---
+++ ansible/roles/init-controller-config/templates/preLocalSettings_allWikis.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * preLocalSettings.php is used to modify all wiki settings. This is where any
+ * any changes to the standard meza configuration should be kept. It also
+ * is where database user and password info is stored
+ **/
+
+
+/**
+ * Enables or disables wiki email capabilities for all wikis, regardless of
+ * of what their individual settings say.
+ *
+ **/
+// disabled by default for now, should be enabled later for
+$mezaEnableAllWikiEmail = true;
+
+// https://www.mediawiki.org/wiki/Manual:$wgPasswordSender
+$wgPasswordSender = "admin@example.com";
+
+// https://www.mediawiki.org/wiki/Manual:$wgEmergencyContact
+$wgEmergencyContact = $wgPasswordSender;
+
+// set a default $mezaAuthType for all wikis that don't specify one
+// $mezaAuthType = 'viewer-read';
