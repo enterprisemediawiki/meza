dbe0b84ec1384bbe3956523d3c93391ee894f33f
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-03-28 18:01:01 -0500

    do import again, but named monolith

--- tests/travis/monolith-from-import.sh
+++ tests/travis/monolith-from-import.sh
@@ -9,25 +9,22 @@ set -eux
 
 fqdn="$1"
 
+# The may be issues with calling the environment anything other than "monolith"
+env_name="monolith"
+
 # Get test "secret" config
 # mkdir /opt/meza/config/local-secret
-# git clone https://github.com/enterprisemediawiki/meza-test-config-secret.git /opt/meza/config/local-secret/imported
+git clone https://github.com/enterprisemediawiki/meza-test-config-secret.git "/opt/meza/config/local-secret/$env_name"
 
 # Write the docker containers IP as the FQDN for the test config (the only
 # config setting we can't know ahead of time)
-# sed -r -i "s/INSERT_FQDN/$fqdn/g;" "/opt/meza/config/local-secret/imported/group_vars/all.yml"
-
-# TRYING TO FIGURE OUT WHAT IS BREAKING...TEMPORARILY MAKE IT NOT "from import"
-meza setup env monolith --fqdn="${fqdn}" --db_pass=1234 --enable_email=false --private_net_zone=public
-mv /opt/meza/config/local-secret/monolith /opt/meza/config/local-secret/imported
-meza deploy imported
-
+sed -r -i "s/INSERT_FQDN/$fqdn/g;" "/opt/meza/config/local-secret/$env_name/group_vars/all.yml"
 
 # get backup files
-# git clone https://github.com/jamesmontalvo3/meza-test-backups.git /opt/meza/data/backups/imported
+git clone https://github.com/jamesmontalvo3/meza-test-backups.git "/opt/meza/data/backups/$env_name"
 
-# Deploy "imported" environment with test config
-# meza deploy imported
+# Deploy environment with test config
+meza deploy "$env_name"
 
 # Basic system check
 bash /opt/meza/tests/travis/server-check.sh
@@ -36,10 +33,7 @@ bash /opt/meza/tests/travis/server-check.sh
 curl -L "http://127.0.0.1:8000"
 
 # Top Wiki API test
-# bash /opt/meza/tests/travis/wiki-check.sh "top" "Top Wiki"
-bash /opt/meza/tests/travis/wiki-check.sh "demo" "Demo Wiki"
+bash /opt/meza/tests/travis/wiki-check.sh "top" "Top Wiki"
 
 # Test for imported image
-# bash /opt/meza/tests/travis/image-check.sh "top" "Test_image.png"
-# NOTE: THIS SHOULD FAIL.
-bash /opt/meza/tests/travis/image-check.sh "demo" "Test_image.png"
+bash /opt/meza/tests/travis/image-check.sh "top" "Test_image.png"
