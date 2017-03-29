19c012e5fcf81e5bd15c5aa7e68dfe4e500b1985
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-03-29 07:04:03 -0500

    Imported configs closely match standard monolith

--- tests/travis/monolith-from-import.sh
+++ tests/travis/monolith-from-import.sh
@@ -14,14 +14,14 @@ env_name="monolith"
 
 # Get test "secret" config
 # mkdir /opt/meza/config/local-secret
-git clone https://github.com/enterprisemediawiki/meza-test-config-secret.git "/opt/meza/config/local-secret/$env_name"
+git clone -b test-fix https://github.com/enterprisemediawiki/meza-test-config-secret.git "/opt/meza/config/local-secret/$env_name"
 
 # Write the docker containers IP as the FQDN for the test config (the only
 # config setting we can't know ahead of time)
 sed -r -i "s/INSERT_FQDN/$fqdn/g;" "/opt/meza/config/local-secret/$env_name/group_vars/all.yml"
 
 # get backup files
-git clone https://github.com/jamesmontalvo3/meza-test-backups.git "/opt/meza/data/backups/$env_name"
+# git clone https://github.com/jamesmontalvo3/meza-test-backups.git "/opt/meza/data/backups/$env_name"
 
 # Deploy environment with test config
 meza deploy "$env_name"
@@ -30,10 +30,11 @@ meza deploy "$env_name"
 bash /opt/meza/tests/travis/server-check.sh
 
 # Is Parsoid service running?
+echo "is parsoid running"
 curl -L "http://127.0.0.1:8000"
 
 # Top Wiki API test
-bash /opt/meza/tests/travis/wiki-check.sh "top" "Top Wiki"
+bash /opt/meza/tests/travis/wiki-check.sh "demo" "Top Wiki"
 
 # Test for imported image
-bash /opt/meza/tests/travis/image-check.sh "top" "Test_image.png"
+# bash /opt/meza/tests/travis/image-check.sh "demo" "Test_image.png"
