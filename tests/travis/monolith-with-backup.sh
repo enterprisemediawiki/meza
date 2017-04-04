1f65959243d91f5227cc4614f467d139550e6fa5
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-04-03 22:23:23 -0500

    pass env var for host key checking to docker

--- tests/travis/monolith-with-backup.sh
+++ tests/travis/monolith-with-backup.sh
@@ -29,9 +29,6 @@ docker_exec_1=( "${docker_exec[@]}" )
 # Copy SSH public key from user "meza-ansible" to host
 docker cp "$container_id_1:/home/meza-ansible/.ssh/id_rsa.pub" /tmp/controller.id_rsa.pub
 
-# CONTAINER 1 disable host key checking to not get prompts
-${docker_exec_1[@]} export ANSIBLE_HOST_KEY_CHECKING=False
-
 # CONTAINER 2 is a backup server
 source "$m_meza_host/tests/travis/init-minion.sh"
 docker_ip_2="$docker_ip"
