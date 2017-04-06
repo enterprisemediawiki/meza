efbd1fe1c77c1ee23d4e84f2ffc0cafec9b80ba5
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-04-06 06:36:03 -0500

    turn off host key checking on minions, too

--- tests/travis/init-minion.sh
+++ tests/travis/init-minion.sh
@@ -14,6 +14,9 @@ docker cp /tmp/controller.id_rsa.pub "$container_id:/tmp/meza-ansible.id_rsa.pub
 docker cp "$m_meza_host/src/scripts/ssh-users/setup-minion-user.sh" "$container_id:/tmp/setup-minion-user.sh"
 ${docker_exec[@]} bash /tmp/setup-minion-user.sh
 
+# Turn off host key checking for user meza-ansible, to avoid prompts
+${docker_exec[@]} bash -c 'echo -e "Host *\n   StrictHostKeyChecking no\n   UserKnownHostsFile=/dev/null" > /home/meza-ansible/.ssh/config'
+
 # Allow SSH login
 # WARNING: This is INSECURE and for test environment only
 ${docker_exec[@]} sed -r -i 's/UsePAM yes/UsePAM no/g;' /etc/ssh/sshd_config
