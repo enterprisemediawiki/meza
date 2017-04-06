fa31d8fd95fc84bdf9081609eee44518c868bf1e
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-04-06 12:19:05 -0500

    add tests/docker/run-tests.sh

---
+++ tests/docker/run-tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Run tests intended for Travis in local Docker containers
+
+# -e: kill script if anything fails
+# -u: don't allow undefined variables
+set -eu
+
+test_type="$1"
+m_meza_host="/opt/meza"
+
+
+if [ "$test_type" == "monolith-from-scratch" ]; then
+
+	# Working directory in Travis is the GitHub repo, which is meza. Mount it.
+	source "$m_meza_host/tests/travis/init-container.sh" "${m_meza_host}" "mount"
+
+	${docker_exec[@]} bash /opt/meza/tests/travis/monolith-from-scratch.sh "$docker_ip"
+
+elif [ "$test_type" == "monolith-from-import" ]; then
+
+	# Working directory in Travis is the GitHub repo, which is meza. Mount it.
+	source "$m_meza_host/tests/travis/init-container.sh" "${m_meza_host}" "mount"
+
+	# TEST ANSIBLE SYNTAX. FIXME: syntax check all playbooks
+	${docker_exec[@]} ANSIBLE_CONFIG=/opt/meza/config/core/ansible.cfg ansible-playbook /opt/meza/src/playbooks/site.yml --syntax-check
+
+	${docker_exec[@]} bash /opt/meza/tests/travis/monolith-from-import.sh "$docker_ip"
+
+elif [ "$test_type" == "import-from-remote" ]; then
+
+	env_name=travis
+	source "$m_meza_host/tests/travis/import-from-remote.setup.sh"
+
+elif [ "$test_type" == "backup-to-remote" ]; then
+
+	env_name=travis
+	source "$m_meza_host/tests/travis/backup-to-remote.setup.sh"
+
+else
+	echo "Bad test type: $test_type"
+	exit 1
+fi
