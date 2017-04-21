db4717231d79e0c14e626777b1f3e03864b8bf86
James Montalvo <jamesmontalvo3@gmail.com>
2017-04-20 20:24:39 -0500

    (Re-)enable SAML authentication (#573)

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // if there's a SAML config file, we need to authenticate with SAML, like, now.
-if ( is_file("/opt/meza/config/local/SAMLConfig.php") ) {
+if ( is_file("/opt/meza/config/core/app-ansible/SAMLConfig.php") ) {
 	require_once __DIR__ . '/NonMediaWikiSimpleSamlAuth.php';
 }
 
