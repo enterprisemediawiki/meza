fd6ee7be1ab260859b458c38538667dccb4e0e2d
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-05-02 22:08:25 -0500

    use meza-docker-full:reorg

--- tests/docker/init-controller.sh
+++ tests/docker/init-controller.sh
@@ -4,7 +4,7 @@
 
 
 # Initiate container
-docker_repo="jamesmontalvo3/meza-docker-full:latest"
+docker_repo="jamesmontalvo3/meza-docker-full:reorg"
 source "$m_meza_host/tests/docker/init-container.sh" "none"
 
 
