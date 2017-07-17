aaa2032bb5edcde4cc0753f61061cae00395c827
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-07-17 17:21:41 -0500

    Make sure root password gets overwritten on each deploy of test

--- tests/docker/backup-to-remote.setup.sh
+++ tests/docker/backup-to-remote.setup.sh
@@ -38,6 +38,16 @@ ${docker_exec_1[@]} default_servers="$docker_ip_1" backup_servers="$docker_ip_2"
 	--fqdn="$docker_ip_1" --db_pass=1234 --private_net_zone=public
 
 
+# secret.yml is encrypted. decrypt first, make edits, re-encrypt.
+secret_yml="/opt/conf-meza/secret/$env_name/secret.yml"
+vault_pass="/opt/conf-meza/users/meza-ansible/.vault-pass-$env_name.txt"
+${docker_exec_1[@]} bash -c "ansible-vault decrypt $secret_yml --vault-password-file $vault_pass"
+${docker_exec_1[@]} bash -c "echo -e '\n' >> $secret_yml"
+${docker_exec_1[@]} bash -c "echo 'mysql_root_password_update: yes' >> $secret_yml"
+${docker_exec_1[@]} bash -c "echo -e '\n' >> $secret_yml"
+${docker_exec_1[@]} bash -c "ansible-vault encrypt $secret_yml --vault-password-file $vault_pass"
+
+
 # Run script on controller to `meza deploy`, `meza create wiki` and
 # `meza backup`
 ${docker_exec_1[@]} bash /opt/meza/tests/deploys/backup-to-remote.controller.sh "$env_name"
