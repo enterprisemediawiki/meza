2723dfe859cfe5de8955d49209e419267ce7a3d9
Greg Rundlett <greg@freephile.com>
2018-04-29 12:25:03 -0400

    Adds ability to run script with local file

--- src/scripts/importExtensions.php
+++ src/scripts/importExtensions.php
@@ -1,5 +1,18 @@
 <?php
 error_reporting(E_ALL);
+
+$HELP = <<<HERE
+Supply the full URL to your wiki API as the first argument. e.g.
+https://freephile.org/w/api.php to get a list of extensions which still need
+to be added locally.
+
+If the Wiki API is access restricted, then execute a
+'?action=query&meta=siteinfo&siprop=extensions&format=json' API query in that
+environment and save the results to a file with a .json extension.
+
+Then supply the path to that file as the argument to this function.
+HERE;
+
 # normalize names by removing any spaces
 $core  = `grep -Po 'name\: (.*)' /opt/meza/config/core/MezaCoreExtensions.yml | sort | awk --field-separator=: '{print $2}' | sed s'/[ \t]*//g'`;
 $local = `grep -Po 'name\: (.*)' /opt/conf-meza/public/MezaLocalExtensions.yml | sort | awk --field-separator=: '{print $2}' | sed s'/[ \t]*//g'`;
@@ -8,26 +21,30 @@ print "Here are the extensions in core\n$core";
 
 print "\n\n Here are additional extensions from MezaLocalExtensions.yml\n$local";
 
+// $core is Meza, and $local are any local additions found in MezaLocalExtensions
 $core = explode("\n", trim($core));
 $local = explode("\n", trim($local));
-
-// var_dump($core);
 
 $api = $argv[1];
 
 if ( ($api == null) || ($api == false) || ($api == '') ) {
-  exit (' Supply the full URL to your wiki API as the first argument. e.g. https://freephile.org/w/api.php to get a list of extensions which still need to be added locally' );
+  fwrite(STDERR, $HELP);
+  exit(1);
 }
-$endpoint = $api . '?action=query&meta=siteinfo&siprop=extensions&format=json';
-//$extension_json = file_get_contents( 'https://freephile.org/w/api.php?action=query&meta=siteinfo&siprop=extensions&format=json' );
+
+if ( substr($api, -3) == 'api' ) {
+  $endpoint = $api . '?action=query&meta=siteinfo&siprop=extensions&format=json';
+} else if ( substr($api, -4) == 'json' ) {
+  $endpoint = $api;
+} else {
+  fwrite(STDERR, $HELP);
+  exit(2);
+}
+
 $extension_json = file_get_contents( $endpoint );
-// $extension_json = file_get_contents( $api );
-
-$extension_json = utf8_encode( $extension_json ); // for bad old implementations where a BOM may exist
-
+// for bad old implementations where a BOM may exist
+$extension_json = utf8_encode( $extension_json );
 $extension_php = json_decode ( $extension_json );
-
-
 // print_r ($extension_php->{'query'}->{'extensions'});
 $extension_php = $extension_php->{'query'}->{'extensions'};
 
@@ -36,22 +53,18 @@ function cmp( $a , $b ) {
 }
 
 function nameFirst( $a, $b ) {
-//  echo "comparing $a with $b ";
   if ($a == 'name') {
     $return = -1; // if we make it 'less' it will be first in the array
-  } else if ($b == 'name') { 
+  } else if ($b == 'name') {
     $return = 1;
   } else {
     $return = 0;
   }
-//  echo "Returning $return\n";
   return $return;
 }
 // usort will sort the array in place
 // $extension_php is an array, each member is an object
 usort( $extension_php, 'cmp' );
-// print_r ($extension_php); die();
-// print "found " . count($extension_php) . " extensions\n";
 
 $names = array();
 $extensions = array();
@@ -60,15 +73,16 @@ foreach ( $extension_php as $ext ) {
     $extensions[] = (array) $ext; // cast each object to an array
 }
 
-
-echo "Comparing " . count( $names ) . " extensions found at $api with the " . count( $core ) . " extensions in core and the " . count( $local ) . " found locally.\n";
+echo "Comparing " . count( $names ) . " extensions found at $api with the " .
+  count( $core ) . " extensions in core and the " . count( $local ) .
+  " found locally.\n";
 $unhandled = array_diff( $names, $core, $local );
 $incore = array_intersect( $names, $core );
-$inqb = array_intersect( $names, $local );
+$inlocal = array_intersect( $names, $local );
 
-echo count( $incore ) . " handled by Core\n";
-echo count( $inqb ) . " handled by QualityBox\n";
-echo "Left with " . count( $unhandled ) . " not in Core Meza or QualityBox\n";
+echo count( $incore ) . " handled by Meza Core\n";
+echo count( $inlocal ) . " handled locally\n";
+echo "Left with " . count( $unhandled ) . " not in Core Meza or Locally\n";
 
 
 /**
@@ -90,28 +104,17 @@ These are the possible keys that you'll see
     [21] => namemsg
     [29] => description
 */
-//$allkeys = array();
-// var_dump($extension_php); exit();
-// $extension_php is an array of objects. each with several properties
 foreach ( $extensions as $ext ) {
- // echo "sorting ${ext['name']}\n";
- // uksort( $ext, 'nameFirst' );    
   if ( in_array( $ext['name'], $unhandled ) ) {
     echo "\n- CUSTOM\n";
   } elseif ( in_array( $ext['name'], $incore ) ) {
     echo "\n- Core\n";
-  } elseif ( in_array( $ext['name'], $inqb ) ) {
-    echo "\n- QualityBox\n";
+} elseif ( in_array( $ext['name'], $inlocal ) ) {
+    echo "\n- Local\n";
   }
   foreach ( $ext as $k => $v ) {
-//    $allkeys[] = $k;
+    // sometimes the values have spurious newlines
     $v = trim($v);
     echo "  $k : $v\n";
   }
 }
-// $allkeys = array_unique($allkeys, SORT_LOCALE_STRING);
-// print_r($allkeys);
-
-// $existing = yaml_parse_file( '/opt/meza/config/core/MezaCoreExtensions.yml' );
-
-// var_dump ($existing);
