f4ca2c8d297a305e7c42e75b2b80f8c8774979c0
Greg Rundlett <greg@equality-tech.com>
2018-04-29 15:54:50 +0000

    messy addition to work with a local file

--- src/scripts/importExtensions.php
+++ src/scripts/importExtensions.php
@@ -19,8 +19,9 @@ if ( ($api == null) || ($api == false) || ($api == '') ) {
   exit (' Supply the full URL to your wiki API as the first argument. e.g. https://freephile.org/w/api.php to get a list of extensions which still need to be added locally' );
 }
 $endpoint = $api . '?action=query&meta=siteinfo&siprop=extensions&format=json';
-// $extension_json = file_get_contents( 'https://freephile.org/w/api.php?action=query&meta=siteinfo&siprop=extensions&format=json' );
+//$extension_json = file_get_contents( 'https://freephile.org/w/api.php?action=query&meta=siteinfo&siprop=extensions&format=json' );
 $extension_json = file_get_contents( $endpoint );
+// $extension_json = file_get_contents( $api );
 
 $extension_json = utf8_encode( $extension_json ); // for bad old implementations where a BOM may exist
 
@@ -33,32 +34,83 @@ $extension_php = $extension_php->{'query'}->{'extensions'};
 function cmp( $a , $b ) {
   return strcmp( $a->name, $b->name );
 }
-// usort will sort the array in place
-usort( $extension_php, 'cmp' );
 
-// print_r ($extension_php);
+function nameFirst( $a, $b ) {
+//  echo "comparing $a with $b ";
+  if ($a == 'name') {
+    $return = -1; // if we make it 'less' it will be first in the array
+  } else if ($b == 'name') { 
+    $return = 1;
+  } else {
+    $return = 0;
+  }
+//  echo "Returning $return\n";
+  return $return;
+}
+// usort will sort the array in place
+// $extension_php is an array, each member is an object
+usort( $extension_php, 'cmp' );
+// print_r ($extension_php); die();
 // print "found " . count($extension_php) . " extensions\n";
 
 $names = array();
+$extensions = array();
 foreach ( $extension_php as $ext ) {
-    $names[] = str_replace( " ", "", $ext->{name} );
+    $names[] = str_replace( " ", "", $ext->{"name"} );
+    $extensions[] = (array) $ext; // cast each object to an array
 }
 
 
 echo "Comparing " . count( $names ) . " extensions found at $api with the " . count( $core ) . " extensions in core and the " . count( $local ) . " found locally.\n";
 $unhandled = array_diff( $names, $core, $local );
-echo "Left with " . count( $unhandled ) . " not in core or local\n";
+$incore = array_intersect( $names, $core );
+$inqb = array_intersect( $names, $local );
+
+echo count( $incore ) . " handled by Core\n";
+echo count( $inqb ) . " handled by QualityBox\n";
+echo "Left with " . count( $unhandled ) . " not in Core Meza or QualityBox\n";
 
 
-
-foreach ( $extension_php as $ext ) {
-  if ( in_array( $ext->name, $unhandled ) ) {
-    echo "-";
-    echo " name: $ext->name\n";
-    echo "  repo: " . $ext->{"vcs-url"} . "\n";
-    echo $ext->{"vcs-version"} ? '  version: ' . $ext->{"vcs-version"} ."\n" : "  version: $ext->version\n";
+/**
+Although all extensions won't define each of these keys,
+These are the possible keys that you'll see
+    [0] => type
+    [1] => name
+    [2] => descriptionmsg
+    [3] => author
+    [4] => url
+    [5] => version
+    [12] => vcs-system
+    [13] => vcs-version
+    [14] => vcs-url
+    [15] => vcs-date
+    [16] => license-name
+    [17] => license
+    [18] => credits
+    [21] => namemsg
+    [29] => description
+*/
+//$allkeys = array();
+// var_dump($extension_php); exit();
+// $extension_php is an array of objects. each with several properties
+foreach ( $extensions as $ext ) {
+ // echo "sorting ${ext['name']}\n";
+ // uksort( $ext, 'nameFirst' );    
+  if ( in_array( $ext['name'], $unhandled ) ) {
+    echo "\n- CUSTOM\n";
+  } elseif ( in_array( $ext['name'], $incore ) ) {
+    echo "\n- Core\n";
+  } elseif ( in_array( $ext['name'], $inqb ) ) {
+    echo "\n- QualityBox\n";
+  }
+  foreach ( $ext as $k => $v ) {
+//    $allkeys[] = $k;
+    $v = trim($v);
+    echo "  $k : $v\n";
   }
 }
+// $allkeys = array_unique($allkeys, SORT_LOCALE_STRING);
+// print_r($allkeys);
 
 // $existing = yaml_parse_file( '/opt/meza/config/core/MezaCoreExtensions.yml' );
 
