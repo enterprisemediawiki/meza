bf0ea8dec9caa180612fc0b0d4b2931376a18ea3
James Montalvo <edwin.j.montalvo@nasa.gov>
2023-03-10 13:02:12 -0600

    fix: capitalize Loader

--- src/scripts/meza.py
+++ src/scripts/meza.py
@@ -38,7 +38,7 @@ signal.signal(signal.SIGINT, sigint_handler)
 def load_yaml ( filepath ):
 	with open(filepath, 'r') as stream:
 		try:
-			return yaml.load(stream, Loader=yaml.loader)
+			return yaml.load(stream, Loader=yaml.Loader)
 		except yaml.YAMLError as exc:
 			print(exc)
 
